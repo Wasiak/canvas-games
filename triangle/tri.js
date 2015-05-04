186ef8b04e8ed30d54d9105ed328384d1f97cac0
Wasiak <qbawasiak@wp.pl>
2015-05-04 21:54:30 +0200

    mouse moving corner

--- triangle/tri.js
+++ triangle/tri.js
@@ -21,3 +21,29 @@ var drawTriangle = function(a, b, c) {
   ctx.stroke();
 }
 drawTriangle(a, b, c);
+
+// just for test
+var makeBigger = function() {
+  clear();
+  a[0]++;
+  drawTriangle(a, b, c);
+  console.log('A= ', a[0], ',', a[1], ', B= ', b[0], ',', b[1], ', C= ', c[0], ',', c[1]);
+}
+// setInterval(makeBigger, 500);
+
+function readMouseMove(e){
+  var result_x = document.getElementById('x');
+  var result_y = document.getElementById('y');
+  result_x.innerHTML = e.clientX - 8;
+  result_y.innerHTML = e.clientY - 8;
+}
+canvas.onmousemove = readMouseMove;
+
+var moveCorner = function(e) {
+  clear();
+  a = [e.clientX -8, e.clientY -8];
+  drawTriangle(a, b, c);
+}
+
+canvas.onmousemove = moveCorner;
+
