97cdf4fc546df08e770b6b11468cc9428845263a
Wasiak <qbawasiak@wp.pl>
2015-05-04 21:11:00 +0200

    function drawing triangle

--- triangle/tri.js
+++ triangle/tri.js
@@ -1,7 +1,23 @@
 var canvas = document.getElementById('canvas');
-ctx = canvas.getContext('2d');
+var ctx = canvas.getContext('2d');
+canvas.width = 500;
+canvas.height = 500;
 
 var clear = function() {
   ctx.fillStyle = '#fff';
   ctx.fillRect(0, 0, 500, 500);
 }
+var a = [200, 200];
+var b = [200, 300];
+var c = [300, 300];
+
+ctx.strokeStyle = '#f40101';
+var drawTriangle = function(a, b, c) {
+  ctx.beginPath();
+  ctx.moveTo(a[0], a[1]);
+  ctx.lineTo(b[0], b[1]);
+  ctx.lineTo(c[0], c[1]);
+  ctx.closePath();
+  ctx.stroke();
+}
+drawTriangle(a, b, c);
