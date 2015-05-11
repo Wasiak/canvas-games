0c87f61bc904d1a1fcaf97ce1914d0aea04f0462
Wasiak <qbawasiak@wp.pl>
2015-05-11 17:32:04 +0200

    drawing altitude in first quater

--- triangle/tri.js
+++ triangle/tri.js
@@ -84,16 +84,41 @@ var drawHeightA = function() {
   var xx = [c[0],a[1]];
   var xxA = c[0] - a[0];
   var xxC = c[1] - a [1];
-  console.log('xx = ', xx);
-  console.log('xxA = ', xxA);
-  console.log('xxC = ', xxC);
-  // Psi is an angle beetwen X axis and AC side of triangle
+  // console.log('xx = ', xx);
+  // console.log('xxA = ', xxA);
+  // console.log('xxC = ', xxC);
+  // Psi is an angle between X axis and AC side of triangle
   var tgPsi = xxC / xxA;
-  console.log('tgPsi = ', tgPsi);
+  // console.log('tgPsi = ', tgPsi);
   //psi in degrees
   var psi = (Math.atan(tgPsi) * 180 / Math.PI);
-  console.log('psi = ', psi);
+  // console.log('psi = ', psi);
+  var sideA = Math.sqrt(Math.pow(c[0] - b[0], 2) + Math.pow(c[1] - b[1], 2));
+  var sideB = Math.sqrt(Math.pow(c[0] - a[0], 2) + Math.pow(c[1] - a[1], 2)); 
+  var sideC = Math.sqrt(Math.pow(b[0] - a[0], 2) + Math.pow(b[1] - a[1], 2));
+  // console.log ('bok a = ', sideA);
+  // console.log ('bok b = ', sideB);
+  // console.log ('bok c = ', sideC);
+  // s = (a+b+c)/2 
+  var s = (sideA + sideB + sideC)/2;
+  var h = (2* Math.sqrt(s*(s-sideA)*(s-sideB)*(s-sideC)))/sideA;
+  console.log('wysokosc = ', h);
+  //eta is angle between sideB and h
+  var cosEta = h / sideB;
+  var eta = (Math.acos(cosEta) * 180 / Math.PI);
+  console.log('eta = ', eta);
 
+  //now draw line with same angle for test
+  ctx.save();
+  ctx.strokeStyle = 'rgb(0, 255, 0)';
+  ctx.beginPath();
+  ctx.moveTo(a[0], a[1]);
+  ctx.translate(a[0], a[1]);
+  ctx.rotate((Math.PI/180) * (psi+eta));  
+  ctx.lineTo(h, 0);
+  ctx.closePath();
+  ctx.stroke();
+  ctx.restore();
 };
 
 canvas.addEventListener('mousedown', makeActiveCorner);
@@ -112,6 +137,7 @@ var moveCorner = function(e) {
         c = [e.clientX - 8, e.clientY - 8];
       }
       drawTriangle(a, b, c);
+      drawHeightA();
     }
   }
 
